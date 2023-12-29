a778124057bbe7fafaa0fa708e731f261a24de64
cattmallahan <cattmallahan@gmail.com>
2023-12-29 12:45:43 -0500

    Update and rename timecodereader_v1.js to timecodereader_v2.js

--- timecodereader_v2.js
+++ timecodereader_v2.js
@@ -796,6 +796,7 @@
                   var date = new Date(0);
                   date.setSeconds(time); // specify value for SECONDS here
                   var timeString = date.toISOString().substring(11, 19);
+                  console.log(timeString);
                   return timeString;
                   
 
@@ -806,6 +807,7 @@
                   var date = new Date(0);
                   date.setSeconds(videolength); // specify value for SECONDS here
                   var timeString = date.toISOString().substring(11, 19);
+                  console.log(timeString);
                   return timeString;
                   
 
