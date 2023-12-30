5639fe22d8d69e18d8fe692ea3f0615cbed3db4a
cattmallahan <cattmallahan@gmail.com>
2023-12-30 13:45:51 -0500

    Update and rename timecodereader_v28.js to timecodereader_v29.js

--- timecodereader_v29.js
+++ timecodereader_v29.js
@@ -670,7 +670,6 @@
                      // clearTimeout(timeout);
                      // timeout = setTimeout(restartVideoSection, duration * 1000);
                       fillSlider(fromSlider, toSlider, '#C1C1C1', '#EB3323', toSlider);
-                      updatelooptimedivs();
 
 
 
@@ -679,7 +678,6 @@
                   function updatestartendkeepplaying(from, to) {
                       section.start = from;
                       section.end = to;
-                      updatelooptimedivs();
 
 
                       //var duration = (section.end - section.start)/(rate) + difference;
@@ -887,7 +885,6 @@
                     toInput.value = section.end;
                     fromSlider.value = section.start;
                     fromInput.value = section.start;
-                    updatelooptimedivs();
 
 
                   }
